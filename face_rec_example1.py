bbcf3a3e8be5601f86aa2efa9b24173f1c6ebddb
Anson Melick <aMelick@moviarobotics.com>
2021-11-17 22:10:53 -0500

    init commit of working primitive face_rec

---
+++ face_rec_example1.py
@@ -0,0 +1,59 @@
+import face_recognition
+import os
+import cv2
+
+KNOWN_FACES_DIR = "known_faces"
+UNKNOWN_FACES_DIR = "unknown_faces"
+TOLERANCE = 0.6  # default is 0.6
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+MODEL = "hog"  # can also use 'hog' cnn runs slower on only cpu than hog
+
+print("loading known faces:")
+
+known_faces = []
+known_names = []
+
+for name in os.listdir(KNOWN_FACES_DIR):
+    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
+        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+        encoding = face_recognition.face_encodings(image)[0]
+        known_faces.append(encoding)
+        known_names.append(name)
+    print("Finished training:", name)
+
+print("processing unknown faces:")
+
+for filename in os.listdir(UNKNOWN_FACES_DIR):
+    print(filename)
+    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+    locations = face_recognition.face_locations(image, model=MODEL)  # location(s) of faces in image
+    encodings = face_recognition.face_encodings(image, locations)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    for face_encoding, face_location in zip(encodings, locations):
+        results = face_recognition.compare_faces(known_faces, face_encoding, tolerance=TOLERANCE)
+        # results is comparison of cur encoding to every face and then return
+        # booleans of if face is here or not based on tolerance
+        match = None
+        if True in results:
+            match = known_names[results.index(True)]
+            print(f"Match found: {match}")
+
+            # Drawing rectangle:
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+            color = [0, 255, 0]  # color of box
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2]+22)
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED) # second box to house text
+            cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THICKNESS)
+
+    cv2.imshow(filename, image)  # Show name of file and then display image
+    cv2.waitKey(0)  # miliseconds so this is 10 seconds: 10000
+    cv2.destroyWindow(filename)
+
+
+
